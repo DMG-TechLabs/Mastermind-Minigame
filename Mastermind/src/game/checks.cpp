076a7e68a36e7fc76344a001f63f42fadf224a53
Konstantinos Mokas <kostasmokas12@gmail.com>
2023-05-29 19:29:55 +0300

    checks last error fix

--- Mastermind/src/game/checks.cpp
+++ Mastermind/src/game/checks.cpp
@@ -1,19 +1,39 @@
 #include "checks.h"
 #include <iostream>
 
+int stringFind(string str, char key){
+    int found=0;
+    for(int i=0;i<str.size();i++){
+        if(str.at(i) == key) found++;
+    }
+
+    return found;
+}
+
 vector<int> checks(string key, string user_try){
     int string_len = key.length();
     int full_correct[string_len];
     int correct = 0;
+    int found;
+    int times[string_len];
 
     for(int i=0;i<string_len;i++){
         full_correct[i] = 0;
-        if(key.at(i) == user_try.at(i)) full_correct[i] = 1; 
+        times[i] = 0;
+        if(key.at(i) == user_try.at(i)) {
+            full_correct[i] = 1; 
+            times[i] = 1;
+        }
     }
 
+
     for(int i=0;i<string_len;i++){
+        found = stringFind(key,key.at(i));
         for(int j=0;j<string_len;j++){
-            if(user_try.at(i) == key.at(j) && full_correct[j] != 1) correct++;
+            if(user_try.at(j) == key.at(i) && full_correct[j] != 1 && times[i]<found) {
+                correct++;
+                times[i]++;
+            }
         }
     }
     
